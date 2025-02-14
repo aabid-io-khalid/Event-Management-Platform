d57258b24515ba8ac55f1759817070406016d565
alid404 <chainsawman.11294@hotmail.com>
2025-02-14 22:54:22 +0100

    create the code of the 2025-02-14T22:54:22+01:00 to handle the the project

---
+++ app/views/front-office/gallery.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   
+<!-- Mirrored from demo.themewinter.com/html/exhibz/gallery.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Aug 2024 15:01:26 GMT -->
+<head>
+      <!-- Basic Page Needs ================================================== -->
+      <meta charset="utf-8">
+
+      <!-- Mobile Specific Metas ================================================== -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+      <!-- Site Title -->
+      <title>Exhibit - Conference &amp; Event HTML Template</title>
+      
+         <!-- CSS
+         ================================================== -->
+   <!-- Bootstrap -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+
+   <!-- FontAwesome -->
+   <link rel="stylesheet" href="css/font-awesome.min.css">
+   <!-- Animation -->
+   <link rel="stylesheet" href="css/animate.css">
+   <!-- magnific -->
+   <link rel="stylesheet" href="css/magnific-popup.css">
+   <!-- carousel -->
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <!-- isotop -->
+   <link rel="stylesheet" href="css/isotop.css">
+   <!-- ico fonts -->
+   <link rel="stylesheet" href="css/xsIcon.css">
+   <!-- Template styles-->
+   <link rel="stylesheet" href="css/style.css">
+   <!-- Responsive styles-->
+   <link rel="stylesheet" href="css/responsive.css">
+
+      <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+   </head>
+
+   <body>
+      <div class="body-inner">
+      <!-- Header start -->
+      <?php require_once 'header.php' ?>
+      <!--/ Header end -->
+
+		<div id="page-banner-area" class="page-banner-area" style="background-image:url(images/hero_area/banner_bg.jpg)">
+			<!-- Subpage title start -->
+			<div class="page-banner-title">
+					<div class="text-center">
+                  <h2>Event Gallery</h2>
+                  <ol class="breadcrumb">
+                     <li>
+                        <a href="#">Exibit /</a>
+                     </li>
+                     <li>
+                           Gallery
+                     </li>
+                  </ol>
+				</div>
+			</div><!-- Subpage title end -->
+		</div><!-- Page Banner end -->
+
+		<section id="main-container" class="main-container ts-gallery">
+			<div class="container">
+					<div class="row">
+							<div class="col-lg-8 mx-auto">
+									<h2 class="section-title text-center">
+											<span>Previus Moments</span>
+											Event Gallery
+									</h2>
+							</div><!-- col end-->
+					</div>
+
+				<div class="grid ts-grid-item">
+					<div class="grid-item">
+					  <a href="images/gallery/gallery1.jpg" class="ts-popup"><img src="images/gallery/gallery1.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery2.jpg" class="ts-popup"><img src="images/gallery/gallery2.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery3.jpg" class="ts-popup"><img src="images/gallery/gallery3.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery4.jpg" class="ts-popup"><img src="images/gallery/gallery4.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery5.jpg" class="ts-popup"><img src="images/gallery/gallery5.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery6.jpg" class="ts-popup"><img src="images/gallery/gallery6.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery7.jpg" class="ts-popup"><img src="images/gallery/gallery7.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery8.jpg" class="ts-popup"><img src="images/gallery/gallery8.jpg" alt="" /></a>
+					</div>
+					<div class="grid-item">
+                  <a href="images/gallery/gallery9.jpg" class="ts-popup"><img src="images/gallery/gallery9.jpg" alt="" /></a>
+					</div>
+				 </div>
+
+
+			</div><!-- Conatiner end -->
+		</section><!-- Main container end -->
+
+		<!-- ts footer area start-->
+    <?php require_once 'footer.php' ?>
+		<!-- ts footer area end-->
+		
+
+      
+        
+    
+
+      <!-- Javascript Files
+            ================================================== -->
+      <!-- initialize jQuery Library -->
+      <script src="js/jquery.js"></script>
+
+      <script src="js/popper.min.js"></script>
+      <!-- Bootstrap jQuery -->
+      <script src="js/bootstrap.min.js"></script>
+      <!-- Counter -->
+      <script src="js/jquery.appear.min.js"></script>
+      <!-- Countdown -->
+      <script src="js/jquery.jCounter.js"></script>
+      <!-- magnific-popup -->
+      <script src="js/jquery.magnific-popup.min.js"></script>
+      <!-- carousel -->
+      <script src="js/owl.carousel.min.js"></script>
+      <!-- Waypoints -->
+      <script src="js/wow.min.js"></script>
+      <!-- isotop -->
+      <script src="js/isotope.pkgd.min.js"></script>
+
+      <!-- Template custom -->
+      <script src="js/main.js"></script>
+
+
+      </div>
+      <!-- Body inner end -->
+   </body>
+
+<!-- Mirrored from demo.themewinter.com/html/exhibz/gallery.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Aug 2024 15:01:28 GMT -->
+</html>
